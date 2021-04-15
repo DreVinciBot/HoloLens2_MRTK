623f1b80fc482ca2224f8efcc2c7a69818ba8121
Andre Cleaver <andre.cleaver@tufts.edu>
2021-04-15 01:01:52 -0400

    created new project with practice use of buttons

---
+++ Assets/Scripts/scene_manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_manager : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+
+    public void SceneA()
+    {
+        SceneManager.LoadScene("SceneA");
+    }
+
+    public void SceneB()
+    {
+        SceneManager.LoadScene("SceneB");
+    }
+
+    public void MRTK_Example()
+    {
+        SceneManager.LoadScene("HandInteractionExamples");
+    }
+
+}
--- HoloLens2Build/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\AClea\Documents\GitHub\HoloLens2_MRTK\HoloLens2Build\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\AClea\Documents\GitHub\HoloLens2_MRTK\HoloLens2Build\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
